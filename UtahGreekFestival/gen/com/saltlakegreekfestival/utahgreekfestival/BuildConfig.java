a68c309790d089cf39179171517e23ee1abe136f
Brad Littler <glittler@gmail.com>
2013-06-11 22:07:16 -0600

    Adding Main

---
+++ UtahGreekFestival/gen/com/saltlakegreekfestival/utahgreekfestival/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.saltlakegreekfestival.utahgreekfestival;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ UtahGreekFestival/src/com/saltlakegreekfestival/utahgreekfestival/MainActivity.java
@@ -0,0 +1,59 @@
+package com.saltlakegreekfestival.utahgreekfestival;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+
+import com.saltlakegreekfestival.utahgreekfestival.BaseActivity;
+import com.jeremyfeinstein.slidingmenu.lib.SlidingMenu;
+
+public class MainActivity extends BaseActivity {
+
+	private Fragment mContent;
+
+	public MainActivity() {
+		super(R.string.greek_festival);
+	}
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		// set the Above View
+		if (savedInstanceState != null)
+			mContent = getSupportFragmentManager().getFragment(savedInstanceState, "mContent");
+		if (mContent == null)
+			mContent = new ColorFragment(R.color.red);	
+
+		// set the Above View
+		setContentView(R.layout.content_frame);
+		getSupportFragmentManager()
+		.beginTransaction()
+		.replace(R.id.content_frame, mContent)
+		.commit();
+
+		// set the Behind View
+		setBehindContentView(R.layout.menu_frame);
+		getSupportFragmentManager()
+		.beginTransaction()
+		.replace(R.id.menu_frame, new ColorMenuFragment())
+		.commit();
+
+		// customize the SlidingMenu
+		getSlidingMenu().setTouchModeAbove(SlidingMenu.TOUCHMODE_FULLSCREEN);
+	}
+
+	@Override
+	public void onSaveInstanceState(Bundle outState) {
+		super.onSaveInstanceState(outState);
+		getSupportFragmentManager().putFragment(outState, "mContent", mContent);
+	}
+
+	public void switchContent(Fragment fragment) {
+		mContent = fragment;
+		getSupportFragmentManager()
+		.beginTransaction()
+		.replace(R.id.content_frame, fragment)
+		.commit();
+		getSlidingMenu().showContent();
+	}
+
+}
